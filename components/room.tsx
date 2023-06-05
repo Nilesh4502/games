23c84180fd55a6f446e7d4ced4ff2b5d5b3904ea
Nilesh4502 <nileshtiw2002@gmail.com>
2023-06-06 00:50:50 +0530

    checkbox and card initialize

--- components/room.tsx
+++ components/room.tsx
@@ -16,6 +16,12 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Checkbox, 
+  CheckboxGroup,
   Button,
   SimpleGrid,
   useMediaQuery,
@@ -32,7 +38,7 @@ interface Room {
 }
 
 function RoomComponent() {
-  const [Category, setCategory] = useState("");
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
   const router = useRouter();
   const [numRooms, setNumRooms] = useState(4);
   const roomContainerRef = useRef(null);
@@ -62,8 +68,14 @@ function RoomComponent() {
     };
   }, []);
 
-  const handleCategorySelect = (category) => {
-    setCategory(category);
+  const handleCategorySelect = (category: string) => {
+    setSelectedCategories((prevSelectedCategories) => {
+      if (prevSelectedCategories.includes(category)) {
+        return prevSelectedCategories.filter((c) => c !== category);
+      } else {
+        return [...prevSelectedCategories, category];
+      }
+    });
   };
 
   const [isLargerThanMobile] = useMediaQuery("(min-width: 768px)");
@@ -72,26 +84,30 @@ function RoomComponent() {
 
   return (
     <div>
-      <Menu>
-        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-          {Category || "Category"}
-        </MenuButton>
-        <MenuList>
-          <MenuItem onClick={() => handleCategorySelect("movies")}>
-            movies
-          </MenuItem>
-          <MenuItem onClick={() => handleCategorySelect("sports")}>
-            sports
-          </MenuItem>
-          <MenuItem onClick={() => handleCategorySelect("games")}>
-            games
-          </MenuItem>
-          <MenuItem onClick={() => handleCategorySelect("music")}>
-            music
-          </MenuItem>
-          <MenuItem onClick={() => router.push("/")}>Home</MenuItem>
-        </MenuList>
-      </Menu>
+        <Menu>
+      <MenuButton as={Button} colorScheme='blue' padding="10px" margin="10px" rightIcon={<ChevronDownIcon />}>
+        Filter
+      </MenuButton>
+      <MenuList minWidth='240px'>
+        <CheckboxGroup
+          value={selectedCategories}
+          onChange={(categories) => setSelectedCategories(categories as string[])}
+        >
+          <Stack spacing={[1, 2]} direction="column">
+            <Checkbox value="movies">Movies</Checkbox>
+            <Checkbox value="sports">Sports</Checkbox>
+            <Checkbox value="games">Games</Checkbox>
+            <Checkbox value="music">Music</Checkbox>
+          </Stack>
+        </CheckboxGroup>
+        <MenuItem onClick={() => setSelectedCategories([])}>
+          Clear Selection
+        </MenuItem>
+        <MenuItem onClick={() => router.push("/")}>
+          Home
+        </MenuItem>
+      </MenuList>
+    </Menu>
       <Flex flexWrap="wrap" justifyContent="center">
         <SimpleGrid
           spacing={4}
@@ -99,7 +115,10 @@ function RoomComponent() {
           width={isLargerThanMobile ? "80%" : "100%"}
         >
           {roomData
-            .filter((item) => item.Room.category === Category)
+            .filter((item) =>
+            selectedCategories.length === 0 ||
+            selectedCategories.includes(item.Room.category)
+          )
             .slice(0, numRooms)
             .map((item) => (
               <Box key={item.Room.id} m="4" p="4" width="100%">
@@ -109,7 +128,7 @@ function RoomComponent() {
                     height="300px"
                     objectFit="cover"
                   />
-                  {/* <video src={item.Room.videoUrl}  height="100px" /> */}
+                 
                   <CardBody>
                     <Stack mt="6" spacing="3">
                       <Heading size="md">{item.Room.type}</Heading>
